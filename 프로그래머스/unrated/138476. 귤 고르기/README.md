42ea1e97d6741bb9daadf1cce75793404abb62ba
HwangMinHyeok <81921470+HwangMinHyeok@users.noreply.github.com>
2022-11-25 15:26:19 +0900

    [unrated] Title: 귤 고르기, Time: 5068.01 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/README.md
+++ 프로그래머스/unrated/138476. 귤 고르기/README.md
@@ -1,6 +1,6 @@
 # [unrated] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
 
 ### 성능 요약
 
--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
@@ -1,23 +1,28 @@
+// * 등장 횟수 큰 놈부터 더해가며 k랑 비교 *
 function solution(k, tangerine) {
-  // tangerineKey: tangerine 중복 제거 배열
-  // cntTangerineKey <- push: tangerine에서 각 tangerineSize의 개수
-  // cntTangerineKey.sort - descending
-  const tangerineKey = Array.from(new Set(tangerine));
-  const cntTangerineKey = [];
-  for (let i = 0; i < tangerineKey.length; i++) {
-    let cnt = 0;
-    for (let j = 0; j < tangerine.length; j++) {
-        if(tangerine[j] === tangerineKey[i]) cnt++;
-    }
-    cntTangerineKey.push(cnt);
-  }
-  cntTangerineKey.sort((a, b) => b - a);
-  // 순서대로 더해가며 k랑 비교
-  // if sum >= k, then return cnt
+  // tangerineKeys = tangerine 중복 제거 배열
+  // 빈 배열 info 에 tangerineKeys의 각 요소의 개수 push
+  // info.sort descending
+  const tangerineKeys = Array.from(new Set(tangerine));
+  const info = [];
+  tangerineKeys.forEach((key) =>
+    info.push(tangerine.filter((size) => size === key).length)
+  );
+  info.sort((a, b) => b - a);
+
+  //   let cnt = 0;
+  //   info.reduce((sum, cur, idx, src) => {
+  //     if (sum >= k) {
+  //       cnt = idx;
+  //       src.splice(idx + 1);
+  //     }
+  //     return sum + cur;
+  //   }, 0);
+
   let cnt = 0;
-  for (let i = 0, sum = 0; i < cntTangerineKey.length; i++) {
-    if (sum >= k) return cnt;
-    sum += cntTangerineKey[i];
+  for (let i = 0, sum = 0; i < info.length; i++) {
+    if (sum >= k) break;
+    sum += info[i];
     cnt++;
   }
   return cnt;

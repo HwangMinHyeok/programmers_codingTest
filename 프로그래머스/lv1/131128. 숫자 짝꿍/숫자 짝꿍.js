a598d20627507e6384673b8dbe09c27f8543c456
HwangMinHyeok <81921470+HwangMinHyeok@users.noreply.github.com>
2022-12-02 02:18:16 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,17 +1,19 @@
 function solution(X, Y) {
+  // X, Y의 각 숫자의 개수를 가진 객체 반환
+  // i=9~0, i가 두 객체에 모두 존재할 때, // for문 종료 후에는 가장 큰 숫자가 result에 저장
+  // 더 적은 value만큼 result = ""에 추가
   const xObj = getNumObj(X);
   const yObj = getNumObj(Y);
   let result = "";
-  for (let i = 0; i <= 9; i++) {
+  for (let i = 9; i >= 0; i--) {
     if (xObj[i] && yObj[i]) {
       let lesser = xObj[i] > yObj[i] ? yObj[i] : xObj[i];
       result += String(i).repeat(lesser);
     }
   }
   if (result.length === 0) return "-1";
-  result = result.split("").reverse();
   if (result[0] === "0" && result[result.length - 1] === "0") return "0";
-  return result.join("");
+  return result;
 }
 
 // 해당 문자열의 각 숫자의 개수

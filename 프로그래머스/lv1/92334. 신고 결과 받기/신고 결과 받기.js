f6db836009fb4c6b480f035181bbeb74e883d30e
HwangMinHyeok <81921470+HwangMinHyeok@users.noreply.github.com>
2022-12-09 09:30:49 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.10 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,42 +1,58 @@
-// a -> b, a -> b
-// a -> c
-// b -> c
+// // a -> b, a -> b
+// // a -> c
+// // b -> c
 
-// complainantObj ={
-//     a: Set {b, c},
-//     b: Set {c}
+// // complainantObj ={
+// //     a: Set {b, c},
+// //     b: Set {c}
+// // }
+// // dependantObj = {
+// //     b: Set {a},
+// //     c: Set {a, b}
+// // }
+
+// function solution(id_list, reports, k) {
+//     const complainantObj = {}; // 신고자 당 피신고자 객체
+//     const dependantObj = {}; // 피신고자 당 신고자 객체
+//     reports.forEach(report => {
+//         const [complainant, dependant] = report.split(" ");
+//         complainantObj[complainant] = !complainantObj[complainant]
+//             ? new Set().add(dependant)
+//             : complainantObj[complainant].add(dependant);
+//         dependantObj[dependant] = !dependantObj[dependant]
+//             ? new Set().add(complainant)
+//             : dependantObj[dependant].add(complainant);
+//     });
+
+//     // complainantObj의 각 신고자 당 피신고자
+//     // id of id_list
+//     // if complainantObj[id] !== undefined -> result.push(cnt)
+//     // else -> result.push(0)
+//     const result = [];
+//     id_list.forEach(id => {
+//         if (complainantObj[id]) {
+//             let cnt = 0;
+//             complainantObj[id].forEach(dependant => {
+//                 if (dependantObj[dependant].size >= k) cnt++;
+//             });
+//             result.push(cnt);
+//         } else result.push(0);
+//     });
+//     return result;
 // }
-// dependantObj = {
-//     b: Set {a},
-//     c: Set {a, b}
-// }
 
-function solution(id_list, reports, k) {
-    const complainantObj = {}; // 신고자 당 피신고자 객체
-    const dependantObj = {}; // 피신고자 당 신고자 객체
-    reports.forEach(report => {
-        const [complainant, dependant] = report.split(" ");
-        complainantObj[complainant] = !complainantObj[complainant]
-            ? new Set().add(dependant)
-            : complainantObj[complainant].add(dependant);
-        dependantObj[dependant] = !dependantObj[dependant]
-            ? new Set().add(complainant)
-            : dependantObj[dependant].add(complainant);
-    });
-
-    // complainantObj의 각 신고자 당 피신고자
-    // id of id_list
-    // if complainantObj[id] !== undefined -> result.push(cnt)
-    // else -> result.push(0)
-    const result = [];
-    id_list.forEach(id => {
-        if (complainantObj[id]) {
-            let cnt = 0;
-            complainantObj[id].forEach(dependant => {
-                if (dependantObj[dependant].size >= k) cnt++;
-            });
-            result.push(cnt);
-        } else result.push(0);
-    });
-    return result;
+function solution(id_list, report, k) {
+    let reports = [...new Set(report)].map(a=>{return a.split(' ')});
+    let counts = new Map();
+    for (const bad of reports){
+        counts.set(bad[1],counts.get(bad[1])+1||1)
+    }
+    let good = new Map();
+    for(const report of reports){
+        if(counts.get(report[1])>=k){
+            good.set(report[0],good.get(report[0])+1||1)
+        }
+    }
+    let answer = id_list.map(a=>good.get(a)||0)
+    return answer;
 }

da0acf6344e8a07c959b838e0fafa53aa51a2aef
HwangMinHyeok <81921470+HwangMinHyeok@users.noreply.github.com>
2022-12-09 09:22:35 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,40 @@
+// // 1
+// // 너무 어렵게 생각한 듯
+// function solution(ingredient) {
+//     // 아래부터, 1 빵 - 2 야채 - 3 고기 - 1 빵
+//     // if 버거 -> splice, bugerCnt++, i = stack.pop() || 0
+//     // else -> 빵 index를 stack에 저장하고 i++
+//     const burger = [1, 2, 3, 1];
+//     let burgerCnt = 0;
+//     let breadIdxStack = [];
+//     for (let i = 0; i <= ingredient.length - 4;) {
+//         if (burger.every((el, idx) => el === ingredient[i + idx])) {
+//             ingredient.splice(i, 4);
+//             burgerCnt++;
+//             i = breadIdxStack.pop() || 0;
+//             continue;
+//         } else {
+//             if (ingredient[i] === 1) breadIdxStack.push(i);
+//             i++;
+//         }
+//     }
+//     return burgerCnt;
+// }
+
+// 2
+// stack에 push하면서 check
+function solution(ingredients) {
+    const burger = [1, 2, 3, 1];
+    let burgerCnt = 0;
+    const ingStack = [];
+    for (const ingredient of ingredients) {
+        ingStack.push(ingredient);
+        if (ingStack.length >= 4) {
+            if (ingStack.slice(-4).every((el, idx) => el === burger[idx])) {
+                burgerCnt++;
+                ingStack.splice(-4);
+            }
+        }
+    }
+    return burgerCnt;
+}

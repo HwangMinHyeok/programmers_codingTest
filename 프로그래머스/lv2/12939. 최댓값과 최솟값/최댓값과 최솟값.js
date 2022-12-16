da3141415cdec5de60f89a2589d2a3052b8a49b0
HwangMinHyeok <81921470+HwangMinHyeok@users.noreply.github.com>
2022-12-16 13:42:19 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,5 +1,4 @@
 function solution(s) {
     const split = s.split(" ");
-    const result = [Math.min(...split), Math.max(...split)];
-    return result.join(" ");
+    return `${Math.min(...split)} ${Math.max(...split)}`
 }

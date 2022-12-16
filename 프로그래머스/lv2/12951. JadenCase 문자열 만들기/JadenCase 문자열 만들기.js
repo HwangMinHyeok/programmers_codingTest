6ffe58aa3c0b43d1ba4edf676f91d3cdc603e602
HwangMinHyeok <81921470+HwangMinHyeok@users.noreply.github.com>
2022-12-16 13:44:44 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -4,7 +4,7 @@ function solution(s) {
   for (const letter of str) {
     // 앞에가 undefined or " "
     // 현재가 알파벳이면
-    if ((result.at(-1) === undefined || result.at(-1) === " ") && isNaN(letter))
+    if ((result.length === 0 || result.at(-1) === " ") && isNaN(letter))
       result += letter.toUpperCase();
     else result += letter;
   }

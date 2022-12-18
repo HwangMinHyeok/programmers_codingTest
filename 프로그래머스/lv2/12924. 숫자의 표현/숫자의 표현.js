311735a5853b78a1b4b3e484b46daddc0d24f594
HwangMinHyeok <81921470+HwangMinHyeok@users.noreply.github.com>
2022-12-19 05:37:50 +0900

    [level 2] Title: 숫자의 표현, Time: 0.22 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,26 @@
+function getDivisors(n) {
+    const result = [];
+    const sqrt = Math.sqrt(n);
+    for (let i = 1; i < sqrt; i++) {
+        if (n % i === 0) result.push(i, n / i);
+    }
+    if (Number.isInteger(sqrt)) result.push(sqrt);
+    return result.sort((a, b) => a - b);
+}
+function solution(n) {
+    let cnt = 0;
+    const divisors = getDivisors(n);
+    // d개 x (n/d) = n
+    divisors.forEach(d => {
+        let eachSum = n / d;
+        // d odd
+        if (d % 2 === 1) {
+            if (eachSum - Math.floor(d / 2) < 1) cnt++;
+        }
+        // eachSum odd
+        if (eachSum % 2 === 1) {
+            if (Math.ceil(eachSum / 2 - d) < 1) cnt++;
+        }
+    });
+    return cnt;
+}

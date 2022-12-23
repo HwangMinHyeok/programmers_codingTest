212cdcd3247e830fb8fee567bd0fe32aa6be9a00
HwangMinHyeok <81921470+HwangMinHyeok@users.noreply.github.com>
2022-12-23 21:12:49 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,11 +1,11 @@
 function solution(n) {
-    const oneCnt = getOneCntFromBinary(n);
-    for (let i = n + 1; i <= 1000001; i++) {
-        if (oneCnt === getOneCntFromBinary(i)) return i;
-    }
-}
-
-function getOneCntFromBinary(n) {
-    const bin = n.toString(2)
-    return bin.match(/1/g).length;
+    const bin = n.toString(2);
+    const oneCnt = bin.match(/1/g).length;
+    const lastOneIdx = bin.lastIndexOf('1');
+    const adder = Math.pow(2, (bin.length - 1) - lastOneIdx);
+    const added = n + adder;
+    const added_oneCnt = added.toString(2).match(/1/g).length;
+    const oneCntDif = oneCnt - added_oneCnt;
+    if (oneCntDif === 0) return added;
+    return added + (Math.pow(2, oneCntDif) - 1);
 }

debd564afc6a13e43dc16e16b13908c2c793db79
HwangMinHyeok <81921470+HwangMinHyeok@users.noreply.github.com>
2022-12-27 16:13:59 +0900

    [level 2] Title: 구명보트, Time: 13.55 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,8 +1,19 @@
+// function solution(people, limit) {
+//     people.sort((a, b) => b - a);
+//     let cnt = 0;
+//     for (let i = 0; i < people.length; i++) {
+//         if (people[i] + people.at(-1) <= limit) people.pop();
+//         cnt++;
+//     }
+//     return cnt;
+// }
+
 function solution(people, limit) {
     people.sort((a, b) => b - a);
+    let popped = 0;
     let cnt = 0;
-    for (let i = 0; i < people.length; i++) {
-        if (people[i] + people.at(-1) <= limit) people.pop();
+    for (let i = 0; i < people.length - popped; i++) {
+        if (people[i] + people.at(-1-popped) <= limit) popped++;
         cnt++;
     }
     return cnt;

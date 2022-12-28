c75d14203b747606491e3c6d4e354be65e40390d
HwangMinHyeok <81921470+HwangMinHyeok@users.noreply.github.com>
2022-12-28 18:15:48 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,12 +1,19 @@
+// 1트: 그냥
+// function solution(n) {
+//     let battery = 0;
+//     while (n !== 1) {
+//         if (n % 2 === 1) {
+//             n--;
+//             battery++;
+//         } else {
+//             n /= 2;
+//         }
+//     }
+//     return battery + 1;
+// }
+
+// 2트: 1트가 2점인가 3점 떠서 더 좋은게 있구나하고 생각하다보니깐
+// 1의 작업이 결국은 n을 이진수로 바꿨을 때 1의 개수 구하는 것과 같다는 걸 알게 됨
 function solution(n) {
-    let battery = 0;
-    while (n !== 1) {
-        if (n % 2 === 1) {
-            n--;
-            battery++;
-        } else {
-            n /= 2;
-        }
-    }
-    return battery + 1;
+    return n.toString(2).match(/1/g).length;
 }

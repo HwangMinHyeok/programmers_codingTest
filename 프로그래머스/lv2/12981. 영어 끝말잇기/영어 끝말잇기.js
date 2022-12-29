5b5ed2a8bc9a42b039b149ede906edb967750e77
HwangMinHyeok <81921470+HwangMinHyeok@users.noreply.github.com>
2022-12-29 10:57:18 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.31 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,6 +1,19 @@
+// 1 slice.includes
+// function solution(n, words) {
+//     for (let i = 1; i < words.length; i++) {
+//         if (words[i - 1].at(-1) !== words[i].at(0) || words.slice(0, i).includes(words[i])) {
+//             return [i % n + 1, Math.floor(i / n) + 1];
+//         }
+//     }
+//     return [0, 0];
+// }
+
+// 2 객체 활용
 function solution(n, words) {
+    const idxObj = {};
+    words.forEach((word, idx) => idxObj[word] !== undefined ? idxObj[word].push(idx) : idxObj[word] = [idx]);
     for (let i = 1; i < words.length; i++) {
-        if (words[i - 1].at(-1) !== words[i].at(0) || words.slice(0, i).includes(words[i])) {
+        if (words[i - 1].at(-1) !== words[i].at(0) || i === idxObj[words[i]].at(1)) {
             return [i % n + 1, Math.floor(i / n) + 1];
         }
     }

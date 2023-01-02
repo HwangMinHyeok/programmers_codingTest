99659d01fbde5ac2aebcde20cd5fc0a59ee894bb
HwangMinHyeok <81921470+HwangMinHyeok@users.noreply.github.com>
2023-01-02 17:49:18 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.53 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,8 +1,13 @@
+// 매 회 1 아님 2칸 -> 피보나치
+// 배열 초기값: [1, 2]
+// 1: n=1일 때, 1가지 방법 - 1
+// 2: n=2일 때, 2가지 방법 - 1,1 / 2
+// fib[i-1]: i칸 이동 방법 수
 function solution(n) {
-  const fib = [0, 1];
-  const getFib = (n) => {
-    if (fib[n] === undefined) fib[n] = getFib(n - 1) + getFib(n - 2);
-    return fib[n] % 1234567;
-  };
-  return getFib(n + 1);
+    const fib = [1, 2];
+    const getFib = (n) => {
+        if (fib[n] === undefined) fib[n] = getFib(n - 1) + getFib(n - 2);
+        return fib[n] % 1234567;
+    };
+    return getFib(n-1);
 }

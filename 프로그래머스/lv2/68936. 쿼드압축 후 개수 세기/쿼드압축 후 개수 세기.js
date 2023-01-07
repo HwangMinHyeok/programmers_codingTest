9e1ebbcab00e328884b672b909a7da4b7cea9d2c
HwangMinHyeok <81921470+HwangMinHyeok@users.noreply.github.com>
2023-01-08 00:57:27 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 8.25 ms, Memory: 41.1 MB -BaekjoonHub

--- 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -1,20 +1,21 @@
-function solution(arr) {
-  const answer = [0, 0];
-  function quadTreeCompression(k, rIdx = 0, cIdx = 0) {
-    // 압축 ㄱㄴ?
-    function canBeCompressed(criteria, size, rowIdx, colIdx) {
-      // 1x1이면 무조건 true
-      if (size === 0) return true;
-      for (let i = rowIdx; i < rowIdx + 2 ** size; i++) {
-        for (let j = colIdx; j < colIdx + 2 ** size; j++) {
-          if (arr[i][j] !== criteria) return false;
-        }
-      }
-      return true;
+// 압축 ㄱㄴ?
+function canBeCompressed(arr, criterion, size, rowIdx, colIdx) {
+  // 1x1이면 무조건 true
+  if (size === 0) return true;
+  for (let i = rowIdx; i < rowIdx + 2 ** size; i++) {
+    for (let j = colIdx; j < colIdx + 2 ** size; j++) {
+      if (arr[i][j] !== criterion) return false;
     }
-    // main
+  }
+  return true;
+}
+
+function solution(arr) {
+  // 압축 ㄱㄴ -> 압축
+  // else -> 재귀
+  function quadTreeCompression(k, rIdx, cIdx) {
     const c = arr[rIdx][cIdx];
-    if (canBeCompressed(c, k, rIdx, cIdx)) answer[c]++;
+    if (canBeCompressed(arr, c, k, rIdx, cIdx)) answer[c]++;
     else {
       for (let i = 0; i < 2; i++) {
         for (let j = 0; j < 2; j++) {
@@ -25,7 +26,8 @@ function solution(arr) {
       }
     }
   }
+  const answer = [0, 0];
   const k = Math.log(arr.length) / Math.log(2); // n=2^k, k=log2(n)
-  quadTreeCompression(k);
+  quadTreeCompression(k, 0, 0);
   return answer;
 }

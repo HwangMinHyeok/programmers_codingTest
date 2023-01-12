657c3979f6be6993bbb337ff2cec2ab63d86fadb
HwangMinHyeok <81921470+HwangMinHyeok@users.noreply.github.com>
2023-01-13 01:34:34 +0900

    [level 2] Title: 프린터, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.js
+++ 프로그래머스/lv2/42587. 프린터/프린터.js
@@ -1,3 +1,9 @@
+// prorities.shift()가 최대값일 때
+//      타겟이면 return printed.length
+//      타겟아니면 location--
+// prorities.shift()가 최대값이 아닐 때
+//      타겟이면 리턴 location = priorities.length - 1 (맨 뒤로 보냄)
+//      타겟아니면 location--
 function solution(priorities, location) {
   const printed = [];
   while (true) {

b697168e9c755be7ec293e75e87261cb1067ee77
HwangMinHyeok <81921470+HwangMinHyeok@users.noreply.github.com>
2023-01-13 11:01:50 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,13 +1,11 @@
-// 돌릴 개수: t.length - pLen + 1
-// 각각 슬라이스 pLen씩
+// 돌릴 개수 forLen: t.length - pLen + 1
+// 각각 슬라이스 p.length 씩
 function solution(t, p) {
-    const pLen = p.length;
-    const pNum = Number(p);
-    const forLen = t.length - pLen + 1;
-    let cnt = 0;
-    for (let i = 0; i < forLen; i++) {
-        const num = Number(t.slice(i, i + pLen));
-        if (num <= pNum) cnt++;
-    }
-    return cnt;
+  const forLen = t.length - p.length + 1;
+  let cnt = 0;
+  for (let i = 0; i < forLen; i++) {
+    const num = Number(t.slice(i, i + p.length));
+    if (num <= Number(p)) cnt++;
+  }
+  return cnt;
 }

f58a84686d464d5b03f09e1288d67bf843f9f8fd
HwangMinHyeok <81921470+HwangMinHyeok@users.noreply.github.com>
2023-01-14 12:06:31 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 1.76 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,10 +1,9 @@
 function solution(s) {
-  const obj = {};
-  const result = [];
-  s = s.split("");
-  for (let i = 0; i < s.length; i++) {
-    result.push(obj[s[i]] === undefined ? -1 : i - obj[s[i]]);
-    obj[s[i]] = i;
-  }
-  return result;
+    const idxMap = new Map();
+    const result = [];
+    s.split("").forEach((el, idx) => {
+        result.push(idx - idxMap.get(el) || -1);
+        idxMap.set(el, idx)
+    });
+    return result;
 }
